367e0164a57aa2763c5b295649a699d40a8a6d47
Vaishnudebi Dutta <39788520+vaishnu7@users.noreply.github.com>
2021-10-21 23:43:05 +0530

    Update LW-SCHEME-func2.m

--- LW-SCHEME-func2.m
+++ LW-SCHEME-func2.m
@@ -1,3 +1,4 @@
+%%
 %LW scheme - function 2
 clc;
 clear;
@@ -6,7 +7,7 @@ format long
 %define variables
 xmin = 0;
 xmax = 1;
-N = 100;
+N = 50;
 dt = 0.009;
 t = 0;
 tmax = 0.1;
@@ -26,8 +27,15 @@ for i = x
     end
     index = index + 1;
 end
-u = u0;
-un = u0;
+u1 = u0;
+u2 = u0;
+u3 = u0;
+u4 = u0;
+un1 = u0;
+un2 = u0;
+un3 = u0;
+un4 = u0;
+
 
 lambda1 = 0.25;
 lambda2 = 0.5;
@@ -40,17 +48,33 @@ for n = 1 : nsteps
  
     %calculate LW scheme
     for i = 2:N+2
-        un(i) = u(i) - (lambda2/2)*(u(i+1)-u(i-1)) + (lambda2^2/2)*(u(i+1)-2*u(i)+u(i-1));
+        un1(i) = u1(i) - (lambda1/2)*(u1(i+1)-u1(i-1)) + (lambda1^2/2)*(u1(i+1)-2*u1(i)+u1(i-1));
+    end
+    for i = 2:N+2
+        un2(i) = u2(i) - (lambda2/2)*(u2(i+1)-u2(i-1)) + (lambda2^2/2)*(u2(i+1)-2*u2(i)+u2(i-1));
+    end
+    for i = 2:N+2
+        un3(i) = u3(i) - (lambda3/2)*(u3(i+1)-u3(i-1)) + (lambda3^2/2)*(u3(i+1)-2*u3(i)+u3(i-1));
+    end
+    for i = 2:N+2
+        un4(i) = u4(i) - (lambda4/2)*(u4(i+1)-u4(i-1)) + (lambda4^2/2)*(u4(i+1)-2*u4(i)+u4(i-1));
     end
     %update
     t = t+dt;
-    u = un;
+    u1 = un1;
+    u2 = un2;
+    u3 = un3;
+    u4 = un4;
     %plot
     plot(x,exact,'r-');
     hold on
-    plot (x,u,'bo-','markerfacecolor','b');
+    plot (x,u1,'--','markerfacecolor','g');
+    plot (x,u2,'--','markerfacecolor','b');
+    plot (x,u3,'--','markerfacecolor','k');
+    plot (x,u4,'--','markerfacecolor','r');
     hold off
-    axis([xmin,xmax,-0.5,1.5])
+    legend('exact sol','lambda = 0.25','lambda = 0.50','lambda = 0.75','lambda = 1.00');
+    axis([xmin,xmax,-1.5,1.5])
     xlabel('x','fontsize',16)
     ylabel('U(t,x)','fontsize',16)
     title(sprintf('time = %1.2f',t),'fontsize',16)
